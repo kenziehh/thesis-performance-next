b09dcbc0042be1b820f55531f524af9652a6ba2f
kenziehh <ghaisani.nurani@gmail.com>
2025-11-10 15:50:24 +0700

    feat: refactor article components to use new data structure and remove JSON file

--- src/features/news/components/article-body.tsx
+++ src/features/news/components/article-body.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import articleData from '../data/article.json'
+import { Article } from '../types'
 
-export default function ArticleBody() {
-    const article = JSON.stringify(articleData);
-    const articleParsed = JSON.parse(article);
+
+export default function ArticleBody({ article }: { article: Article }) {
+
     return (
         <section className="prose max-w-none prose-p:leading-relaxed prose-headings:scroll-mt-20">
-            <h1 className="text-3xl font-bold mb-6">{articleParsed.title}</h1>
-            {articleParsed.subheads.map((section: { heading: string | number | bigint | boolean | React.ReactElement<unknown, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | React.ReactPortal | Promise<string | number | bigint | boolean | React.ReactPortal | React.ReactElement<unknown, string | React.JSXElementConstructor<any>> | Iterable<React.ReactNode> | null | undefined> | null | undefined; paragraphs: any[]; }, index: React.Key | null | undefined) => (
+            <h1 className="text-3xl font-bold mb-6">{article.title}</h1>
+            {article.subheads.map((section, index) => (
                 <div key={index} className="mt-8">
                     <h2 className="mt-8 text-2xl font-semibold text-primary">{section.heading}</h2>
                     {section.paragraphs.map((p, i) => (
